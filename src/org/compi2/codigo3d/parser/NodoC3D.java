db78a426fb9e184e94e5ab553b1f6ac496a75f53
Cristi Vasquez <cv.crisj@gmail.com>
2017-03-08 23:31:37 -0600

    Hoja de trabajo No.3 nombre: Cristi Juana Vásquez Juárez carne: 201114717

--- src/org/compi2/codigo3d/parser/NodoC3D.java
+++ src/org/compi2/codigo3d/parser/NodoC3D.java
@@ -8,9 +8,30 @@ package org.compi2.codigo3d.parser;
  */
 public class NodoC3D {
     private String cad;
+    private String etqV;
+    private String etqF;
 
     public NodoC3D(String cad) {
         this.cad = cad;
+    }
+    
+    public NodoC3D(String etqV, String etqF){
+        this.etqV = etqV;
+        this.etqF = etqF;
+    }
+    
+    public NodoC3D(String etqV, String etqF, String temp){
+        this.etqV = etqV;
+        this.etqF = etqF;
+        this.cad = temp;
+    }
+    
+    public String getEtqV(){
+        return etqV;
+    }
+    
+    public String getEtqF(){
+        return etqF;
     }
     
     public String getCad(){
